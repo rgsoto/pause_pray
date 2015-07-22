cdf002934b02e945cf383c3a58d6d05010ca17e0
Reni <your.rgabrielsoto@gmail.com>
2015-07-21 20:17:16 -0500

    Last stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150701024010) do
+ActiveRecord::Schema.define(version: 20150706031248) do
 
   create_table "prayer_requests", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "prayers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
