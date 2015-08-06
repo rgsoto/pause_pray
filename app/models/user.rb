7357a573da8b1bde219c3872a2d5afcf612a9643
Reni <your.rgabrielsoto@gmail.com>
2015-08-06 08:33:13 -0500

    Added secure password functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ActiveRecord::Base
   validates_presence_of :username, :email
   validates_uniqueness_of :username
-  has_secure_password
   has_many :prayer_requests
   has_many :prayers
+  has_secure_password
+  validates: password, presence: true, length: {minimum: 6}
 end
+
+# from hartl book starting at 6.3 
+#class User < ActiveRecord::Base
+ # before_save { self.email = email.downcase }
+  #validates :name, presence: true, length: { maximum: 50 }
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #validates :email, presence: true, length: { maximum: 255 },
+                    #format: { with: VALID_EMAIL_REGEX },
+                    #uniqueness: { case_sensitive: false }
+  #has_secure_password
+  #validates :password, presence: true, length: { minimum: 6 }
+#end
