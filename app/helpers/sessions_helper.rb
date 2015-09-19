7358184ca64ef5ae32e8eedcf3957e60c4a841c3
Reni <rgabrielsoto@gmail.com>
2015-09-19 15:14:29 -0500

    Added logout funcitonality

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,11 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   # Returns true if the user is logged in, false otherwise. 
   def logged_in?
     !current_user.nil?
