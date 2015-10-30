98e03d199a1e23bb671fb846e47bd877ac79be21
Reni <rgabrielsoto@gmail.com>
2015-10-30 08:52:32 -0500

    https://www.railstutorial.org/book/log_in_log_out#sec-remember_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@
 
 # from hartl book starting at 6.3 
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   validates_uniqueness_of :username
   has_many :prayer_requests
   has_many :prayers
@@ -20,4 +21,22 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  
+
+  # Returns the hash digest of the given string. I MIGHT NEED THIS IF SOMETHING ISNT WORKING
+  #def User.digest(string)
+   # cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    #BCrypt::Password.create(string, cost: cost)
+  #end
+
+  # Returns a random token. 
+  def User.new_token 
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember 
+    self.remember_token =  User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729020021) do
+ActiveRecord::Schema.define(version: 20151030133517) do
 
   create_table "prayer_requests", force: :cascade do |t|
     t.string   "title"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150729020021) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_digest"
   end
 
 end
